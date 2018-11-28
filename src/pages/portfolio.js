f3078458289bec300bc88ac6596b549431cdaafa
Kevin Castro Cruz <kevinclap@Kevins-MacBook-Pro.local>
2018-11-27 22:29:33 -0600

    initial commit

---
+++ src/pages/portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+    <h1>Portfolio</h1>
+    <p>Consequat cillum velit enim sit do enim exercitation aute. 
+      Reprehenderit velit nulla minim do esse commodo pariatur 
+      occaecat ullamco exercitation elit excepteur reprehenderit. 
+      Fugiat irure elit sunt officia veniam pariatur quis eu Lorem. 
+      Consequat excepteur ipsum ipsum nisi adipisicing tempor eu ex labore consectetur. 
+      Quis qui cillum ipsum laborum officia duis ad laborum sunt nulla dolor tempor. 
+      Consectetur ut dolore esse id ex ea exercitation in id culpa sunt non voluptate adipisicing.  </p>
+    </div>
+  )
+}
+
+export default Portfolio;
